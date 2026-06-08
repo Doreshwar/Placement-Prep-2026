44608dc3c5f967eb184ddf483dee8520740c6319
Dora <adityaraghav2005rocks@gmail.com>
2026-06-08 23:45:01 +0530

    Day 7 question 2

---
+++ DSA/LC_242.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length()!=t.length()) return false;
+        unordered_map<char,int> mpp;
+        for (auto c:s) mpp[c]++;
+        for (auto c:t) mpp[c]--;
+        for (auto it:mpp){
+            if (it.second!=0) return false;
+        }
+        return true;
+    }
+};
